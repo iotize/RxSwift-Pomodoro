05cdf9a96584da4f444d5afa0abfc98605d20081
Ryan Davies <iotize@icloud.com>
2018-06-02 23:11:19 +0100

    Remove main storyboard

--- Pomodoro/AppDelegate.swift
+++ Pomodoro/AppDelegate.swift
@@ -2,10 +2,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+    lazy var window: UIWindow? = {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        return window
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 }
