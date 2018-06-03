dc9bc25af2fb2de187e9cd07dbee3a8d7a694183
Ryan Davies <iotize@icloud.com>
2018-06-03 23:37:31 +0100

    Add static progress view

--- Pomodoro/TimerView.swift
+++ Pomodoro/TimerView.swift
@@ -1,4 +1,6 @@
 import UIKit
+import UICircularProgressRing
+import SnapKit
 
 class TimerView: UIView {
     override init(frame: CGRect) {
@@ -10,19 +12,42 @@ class TimerView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    private let progressView: UICircularProgressRingView = {
+        let progressView = UICircularProgressRingView()
+        progressView.innerRingColor = .red
+        progressView.outerRingColor = .white
+        progressView.value = 100
+        progressView.valueIndicator = ""
+        progressView.snp.makeConstraints { make in
+            make.height.equalTo(progressView.snp.width)
+        }
+        return progressView
+    }()
+    
     private let timeRemainingLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.preferredFont(forTextStyle: .body)
         label.text = "1 minute 30 seconds remaining"
         return label
     }()
     
     private func setupSubviews() {
-        addSubview(timeRemainingLabel)
-        NSLayoutConstraint.activate([
-            timeRemainingLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            timeRemainingLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
+        [progressView, timeRemainingLabel].forEach { stackView.addArrangedSubview($0) }
+        addSubview(stackView)
+        progressView.snp.makeConstraints { make in
+            make.leftMargin.equalTo(self.snp.leftMargin)
+            make.rightMargin.equalTo(self.snp.rightMargin)
+        }
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
     }
 }
--- Pomodoro/TimerViewController.swift
+++ Pomodoro/TimerViewController.swift
@@ -1,7 +1,18 @@
 import UIKit
+import SnapKit
 
 class TimerViewController: UIViewController {
-    override func loadView() {
-        self.view = TimerView(frame: .zero)
+    lazy var timerView = TimerView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(timerView)
+        timerView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left)
+            make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right)
+        }
     }
 }
