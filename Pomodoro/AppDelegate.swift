5d8d66a5b9122d3d7e21d2c86f140a648f0a302b
Ryan Davies <iotize@icloud.com>
2018-06-03 23:37:52 +0100

    Add navigation bar

--- Pomodoro/AppDelegate.swift
+++ Pomodoro/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window?.rootViewController = TimerViewController()
+        window?.rootViewController = UINavigationController(rootViewController: TimerViewController())
         window?.makeKeyAndVisible()
         return true
     }
--- Pomodoro/TimerViewController.swift
+++ Pomodoro/TimerViewController.swift
@@ -4,6 +4,15 @@ import SnapKit
 class TimerViewController: UIViewController {
     lazy var timerView = TimerView()
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        title = "Pomodoro"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
